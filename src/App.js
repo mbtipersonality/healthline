99400026248844e3fdbdc57e7a5d9d021bc1eaee
ramdev8877771 <156853367+ramdev8877771@users.noreply.github.com>
2024-04-01 02:08:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import React from 'react'
+import search from './search'
+
+function App() {
+  const handleClick = () => {
+    const randSearch = search[Math.floor(Math.random() * 2997 )]
+
+    window.location = `https://yewtu.be/search?q=${randSearch}`
+  }
+  return (
+    <div className='App' onClick={handleClick}>
+      <h1>Click Anywhere</h1>
+    </div>
+  )
+}
+
+export default App
