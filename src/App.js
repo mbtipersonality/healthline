8c1adebb8134305d9bad405902d61c6a1a6ebe02
ramdev8877771 <156853367+ramdev8877771@users.noreply.github.com>
2024-04-08 14:41:18 +0530

    Update App.js
    removed yewtu comment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   const handleClick = () => {
     const randSearch = search[Math.floor(Math.random() * 2997 )]
 
-    // window.location = `https://yewtu.be/search?q=${randSearch}`
     window.location = `https://www.google.com/search?q=site%3Ahealthline.com+${randSearch}&sca_esv=579769985&rlz=1C1FKPE_enIN956IN956&ei=lOxIZZytJ5mW4-EPp9SWWA`
   }
   return (
